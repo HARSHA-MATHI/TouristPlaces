e3f2e355574832c8f36c061835cb929bafc5aa84
DELL <DELL@HARSHA>
2024-12-27 18:06:45 +0530

    Add project files.

---
+++ TouristPlaces/Controllers/TouristController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TouristPlaces.Dal;
+using TouristPlaces.Models;
+
+namespace TouristPlaces.Controllers
+{
+    public class TouristController : Controller
+    {
+        TouristPlaceContext context = null;
+        public TouristController()
+        {
+            context=new TouristPlaceContext();
+        }
+        public ActionResult UploadTouristPlace()
+        {
+            return View();
+        }
+        public ActionResult SaveTouristPlace(TouristPlace tp)
+        {
+            tp.Files=Request.Files;
+            HttpPostedFileBase file = tp.Files[0];
+            string filepath="~/Images/TouristPlaces/"+file.FileName;
+            tp.ImagePath=filepath;
+            file.SaveAs(Server.MapPath(filepath));
+            context.touristPlaces.Add(tp);
+            context.SaveChanges();
+            List<TouristPlace>tplist=context.touristPlaces.ToList();
+            return View("DisplayTouristPlaces",tplist);
+        }
+       
+        public ActionResult Index()
+        { 
+            return View();  
+        }
+    }
+}
---
+++ TouristPlaces/Dal/TouristPlaceContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using TouristPlaces.Models;
+
+namespace TouristPlaces.Dal
+{
+    public class TouristPlaceContext:DbContext
+    {
+        public TouristPlaceContext()
+        {        
+            
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<TouristPlace>().ToTable("TouristPlace");
+        }
+        public DbSet<TouristPlace> touristPlaces { get; set; }
+    }
+}
